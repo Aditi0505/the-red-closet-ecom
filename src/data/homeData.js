8403ff134e03611e42abc0045fc58da135933306
Aditi0505 <aditi5may1997@gmail.com>
2022-03-29 23:13:53 +0530

    fix: add different categories on home page

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -17,14 +17,14 @@ const cardList = [
   {
     image: "/assets/images/album2.jpg",
     title: "New Arrivals",
-    msg: "Check out the latest accessories collections.",
-    category: "Accessories",
+    msg: "Check out the latest wearables collections.",
+    category: "Wearables",
   },
   {
     image: "/assets/images/taylor-swift-album.jpg",
-    title: "New Albums",
-    msg: "Check out the latest album and grab this limited edition.",
-    category: "Albums",
+    title: "All Categories",
+    msg: "Check out the latest collection and grab the limited edition.",
+    category: "",
   },
 ];
 
