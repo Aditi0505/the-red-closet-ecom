eb76fb2b8892a85ee4ad619ee5c321d4c05f5e7d
Aditi0505 <aditi5may1997@gmail.com>
2022-04-01 02:30:57 +0530

    add: dummy token for api-calls

---
+++ src/token.js
@@ -0,0 +1,4 @@
+// Will remove this after auth implementation
+
+export const encodedToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJhNGFmNzg0My04ZGUwLTRhYjUtOGRlMy1mNzlkMTYyZmQ5ZGIiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.7lDIPNlh8da2ECODaDJXiorm17YFSLGnroDD9ZZDiUY";
