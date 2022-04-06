5b5aa73c71b734cae9e7060f8aeceb0afa98012d
Aditi0505 <aditi5may1997@gmail.com>
2022-04-07 02:25:55 +0530

    fix: toast display issues on no item

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -33,65 +33,69 @@ const Cart = () => {
 
   return (
     <main className="outer-wrapper">
-      <section className="display-screen">
-        {cartState.itemsInCart.map(
-          (item) =>
-            item.quantity > 0 && <CartProduct product={item} key={item._id} />
-        )}
-        {cartState.quantity > 0 && cartState.itemsInCart.length && (
-          <div className="text-card-container card">
-            <div className="card-inner-container">
-              <div className="card-body padding-sm">
-                <div className="card-title text-left">Price Details</div>
-                <div className="card-desc flex-column">
-                  <hr className="full-width" />
-                  <div className="flex-spbt">
+      {cartState.itemsInCart.length > 0 ? (
+        <section className="display-screen">
+          {cartState.itemsInCart.map(
+            (item) =>
+              item.quantity > 0 && <CartProduct product={item} key={item._id} />
+          )}
+          {cartState.quantity > 0 && cartState.itemsInCart.length > 0 && (
+            <div className="text-card-container card">
+              <div className="card-inner-container">
+                <div className="card-body padding-sm">
+                  <div className="card-title text-left">Price Details</div>
+                  <div className="card-desc flex-column">
+                    <hr className="full-width" />
+                    <div className="flex-spbt">
+                      <span className="items">
+                        Price {`(${cartState.itemsInCart.length} items)`}
+                      </span>
+                      <span className="price">{` $${cartState.totalPrice} `}</span>
+                    </div>
+                    <div className="flex-spbt">
+                      <span className="discount">Discount</span>
+                      <span className="price">
+                        - {` $ ${cartState.totalDiscountedPrice} `}
+                      </span>
+                    </div>
+                    <div className="flex-spbt">
+                      <span className="delivery">Delivery Charges</span>
+                      <span className="price">$499</span>
+                    </div>
+                    <hr className="full-width" />
+                    <div className="flex-spbt">
+                      <span className="total">Total Amount</span>
+                      <span className="price">{` $ ${totalCartAmount} `}</span>
+                    </div>
+                    <hr className="full-width" />
                     <span className="items">
-                      Price {`(${cartState.itemsInCart.length} items)`}
+                      You will save {` $ ${cartState.totalDiscountedPrice} `} on
+                      this order
                     </span>
-                    <span className="price">{` $${cartState.totalPrice} `}</span>
+                    <button className="full-width btn btn-primary">
+                      Place Order
+                    </button>
                   </div>
-                  <div className="flex-spbt">
-                    <span className="discount">Discount</span>
-                    <span className="price">
-                      - {` $ ${cartState.totalDiscountedPrice} `}
-                    </span>
-                  </div>
-                  <div className="flex-spbt">
-                    <span className="delivery">Delivery Charges</span>
-                    <span className="price">$499</span>
-                  </div>
-                  <hr className="full-width" />
-                  <div className="flex-spbt">
-                    <span className="total">Total Amount</span>
-                    <span className="price">{` $ ${totalCartAmount} `}</span>
-                  </div>
-                  <hr className="full-width" />
-                  <span className="items">
-                    You will save {` $ ${cartState.totalDiscountedPrice} `} on
-                    this order
-                  </span>
-                  <button className="full-width btn btn-primary">
-                    Place Order
-                  </button>
                 </div>
+                <div className="hide-overlay"> Women</div>
               </div>
-              <div className="hide-overlay"> Women</div>
-            </div>
-            <div className="hide">
-              <button className=" btn-action flex-center">Add to Cart</button>
-              <button className="btn-action-horizontal secondary">
-                Remove from wishlist
-              </button>
-              <span className="dismiss flex-center">
-                <button className="card-btn-close">
-                  <i className="fas fa-times-circle close"></i>
+              <div className="hide">
+                <button className=" btn-action flex-center">Add to Cart</button>
+                <button className="btn-action-horizontal secondary">
+                  Remove from wishlist
                 </button>
-              </span>
+                <span className="dismiss flex-center">
+                  <button className="card-btn-close">
+                    <i className="fas fa-times-circle close"></i>
+                  </button>
+                </span>
+              </div>
             </div>
-          </div>
-        )}
-      </section>
+          )}
+        </section>
+      ) : (
+        ""
+      )}
     </main>
   );
 };
