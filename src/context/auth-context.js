4e5794793b351a81241543de39467afb3cc35ff7
Aditi0505 <aditi5may1997@gmail.com>
2022-04-17 00:34:18 +0530

    feat: add auth context and reducer

---
+++ src/context/auth-context.js
@@ -0,0 +1,35 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { authReducer } from "../reducer/auth-reducer";
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [authState, authDispatch] = useReducer(authReducer, {
+    isLoggedIn: false,
+    isSignedUp: false,
+    isLoggedOut: false,
+    encodedToken: localStorage.getItem("token"),
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.setItem("token", authState.encodedToken);
+  }, [authState.encodedToken]);
+
+  const logoutHandler = () => {
+    authDispatch({ type: "LOGOUT", payload: "" });
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider value={{ authState, authDispatch, logoutHandler }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
