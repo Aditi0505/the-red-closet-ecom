0cd3bd2c1cb45202789ed2b622c940783c8c8567
Aditi0505 <aditi5may1997@gmail.com>
2022-06-19 13:26:50 +0530

    feat: order provider in app file

--- src/App.js
+++ src/App.js
@@ -1,52 +1,113 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, ProductListing, Cart, Wishlist, Login, Signup } from "./pages";
-import { Footer, NavBar, Toast } from "./components";
+import {
+  Home,
+  ProductListing,
+  Cart,
+  Wishlist,
+  Login,
+  Signup,
+  Profile,
+  Checkout,
+} from "./pages";
+import {
+  Address,
+  Footer,
+  NavBar,
+  OrderHistory,
+  Toast,
+  UserInfo,
+} from "./components";
 import {
   ToastProvider,
   FilterProvider,
   CartProvider,
   AuthProvider,
+  OrderProvider,
 } from "./context";
 import Mockman from "mockman-js";
 import { RequiresAuth } from "./components/Auth/RequiresAuth";
 const App = () => {
   return (
     <AuthProvider>
-      <CartProvider>
-        <FilterProvider>
-          <ToastProvider>
-            <Toast />
-            <NavBar />
-            <div className="App">
-              <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/products" element={<ProductListing />} />
+      <OrderProvider>
+        <CartProvider>
+          <FilterProvider>
+            <ToastProvider>
+              <Toast />
+              <NavBar />
+              <div className="App">
+                <Routes>
+                  <Route path="/" exact element={<Home />} />
+                  <Route path="/products" element={<ProductListing />} />
 
-                <Route
-                  path="/cart"
-                  element={
-                    <RequiresAuth>
-                      <Cart />
-                    </RequiresAuth>
-                  }
-                />
-                <Route
-                  path="/wishlist"
-                  element={
-                    <RequiresAuth>
-                      <Wishlist />
-                    </RequiresAuth>
-                  }
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/mock" element={<Mockman />} />
-              </Routes>
-            </div>
-            <Footer />
-          </ToastProvider>
-        </FilterProvider>
-      </CartProvider>
+                  <Route
+                    path="/cart"
+                    element={
+                      <RequiresAuth>
+                        <Cart />
+                      </RequiresAuth>
+                    }
+                  />
+                  <Route
+                    path="/wishlist"
+                    element={
+                      <RequiresAuth>
+                        <Wishlist />
+                      </RequiresAuth>
+                    }
+                  />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/mock" element={<Mockman />} />
+                  <Route
+                    path="/user"
+                    element={
+                      <RequiresAuth>
+                        <Profile />
+                      </RequiresAuth>
+                    }
+                  >
+                    <Route
+                      index
+                      element={
+                        <RequiresAuth>
+                          <UserInfo />
+                        </RequiresAuth>
+                      }
+                    />
+                    <Route
+                      path="addresses"
+                      element={
+                        <RequiresAuth>
+                          <Address />
+                        </RequiresAuth>
+                      }
+                    />
+                    <Route
+                      exact
+                      path="order-history"
+                      element={
+                        <RequiresAuth>
+                          <OrderHistory />
+                        </RequiresAuth>
+                      }
+                    />
+                  </Route>
+                  <Route
+                    path="/checkout"
+                    element={
+                      <RequiresAuth>
+                        <Checkout />
+                      </RequiresAuth>
+                    }
+                  />
+                </Routes>
+              </div>
+              <Footer />
+            </ToastProvider>
+          </FilterProvider>
+        </CartProvider>
+      </OrderProvider>
     </AuthProvider>
   );
 };
