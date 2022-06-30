3fe819ffda2b144262f50b39eb5e400e9da6b5fa
Aditi0505 <aditi5may1997@gmail.com>
2022-06-30 17:55:47 +0530

    feat: add  mobile view for filter and sort

--- src/components/Button/FilterButton.jsx
+++ src/components/Button/FilterButton.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { MobileLayoutFilter } from "../MobileLayout/MobileLayoutFilter";
+import { MobileLayoutSort } from "../MobileLayout/MobileLayoutSort";
+
 const FilterButton = ({ value }) => {
+  const [sort, setSort] = useState(false);
+  const [filter, setFilter] = useState(false);
+  const setFilterAction = (value) => {
+    if (value === "Sort") {
+      setSort((prev) => !prev);
+    } else if (value === "Filter") {
+      setFilter((prev) => !prev);
+    }
+  };
   return (
-    <button className="btn-action-horizontal filter-button secondary">
-      {value}
-    </button>
+    <div className="full-width passwordBox">
+      <button
+        className="btn-action-horizontal full-width secondary"
+        onClick={() => setFilterAction(value)}
+      >
+        {value}
+      </button>
+      {sort ? (
+        <MobileLayoutSort inputHandler={setFilterAction} />
+      ) : filter ? (
+        <MobileLayoutFilter inputHandler={setFilterAction} />
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 export { FilterButton };
