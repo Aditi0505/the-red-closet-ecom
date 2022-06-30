e45d68b829b2992dc889a5ede6419fe6550b6ae6
Aditi0505 <aditi5may1997@gmail.com>
2022-06-30 19:20:42 +0530

    fix: add null coleasing to handle undefined user

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,16 +4,17 @@ const UserInfo = () => {
   const { authState } = useAuth();
   const { user } = authState;
   const userData = JSON.parse(localStorage.getItem("user"));
+  console.log(user, userData);
   return (
     <div className="outer-wrapper">
       <div className="flex-center full-height profile-screen flex-column">
         <div className="flex-column">
           <h1>Your Profile</h1>
           <p className="text-md text-left">{`Name: ${
-            userData.user.firstName ?? user.firstName
-          } ${userData.user.lastName ?? user.lastName}`}</p>
+            userData?.user?.firstName ?? user?.firstName
+          } ${userData?.user?.lastName ?? user?.lastName}`}</p>
           <p className="text-md text-left">{`Email: ${
-            userData.email ?? user.email
+            userData?.user?.email ?? user?.email
           }`}</p>
         </div>
       </div>
